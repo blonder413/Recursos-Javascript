9c006469818cf825a7236c32dd19d4062bdfdb92
Jonathan Morales <blonder413@gmail.com>
2023-12-05 11:02:56 -0500

    navigator

---
+++ navigator.js
@@ -0,0 +1,11 @@
+// validar si las cookies están activadas
+console.log(navigator.cookieEnabled);
+
+// información del navegador
+console.log(navigator.userAgent);
+
+// detectar idioma
+console.log(navigator.language);
+
+// detectar si está conectado a internet
+console.log(navigator.onLine);
