5d67afff752211a454fd8488cf52ab33f1fbb897
Jonathan Morales <blonder413@gmail.com>
2023-12-08 08:37:54 -0500

    fechas

---
+++ apis/fechas.js
@@ -0,0 +1,10 @@
+const fecha = new Date()
+const nacimiento = new Date('1960-09-13T12:00:00')
+
+console.log(fecha.toString());
+console.log(fecha.toDateString());
+console.log(fecha.toTimeString());
+console.log(fecha.getMonth());
+console.log(fecha.getDate());
+console.log(fecha.getDay());
+console.log(fecha.getFullYear());
