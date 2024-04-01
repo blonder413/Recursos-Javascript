63789b52c5397553405c3a575dd5fdc5c8645c8c
Jonathan Morales <blonder413@gmail.com>
2024-04-01 10:13:14 -0500

    document ready sin jquery

---
+++ ready.js
@@ -0,0 +1,6 @@
+/**
+ * Ejecuta el código luego de que ha cargado todo el documento
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Ya se ha cargado el documento');
+});
